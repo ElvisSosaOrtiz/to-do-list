9caa313eddac6c4a5ba3e47143cd5b7695a308cc
elvis <elvissosaortiz27@gmail.com>
2025-02-10 18:50:48 -0400

    refactor: Add documentation information for the service methods

--- backend/RepositoryContracts/ITaskRepository.cs
+++ backend/RepositoryContracts/ITaskRepository.cs
@@ -6,10 +6,38 @@
 
     public interface ITaskRepository
     {
+        /// <summary>
+        /// Adds a task to the database.
+        /// </summary>
+        /// <param name="request">The request body from which the task is created.</param>
+        /// <returns>A reference of the task entity.</returns>
         TaskEntity CreateTask(RequestOfManageTask request);
+
+        /// <summary>
+        /// Removes a task from the database.
+        /// </summary>
+        /// <param name="taskId">The id of the task to remove.</param>
+        /// <returns>A message confirmation of the task removal.</returns>
         string RemoveTask(int taskId);
+
+        /// <summary>
+        /// Retrieves all tasks from the database.
+        /// </summary>
+        /// <returns>A collection of tasks.</returns>
         IQueryable<TaskEntity> GetAllTasks();
+
+        /// <summary>
+        /// Gets a task by it's id.
+        /// </summary>
+        /// <param name="taskId">The id of the task to retrieve.</param>
+        /// <returns>A reference of the task entity.</returns>
         TaskEntity? GetTaskDetails(int taskId);
+
+        /// <summary>
+        /// Modifies the task selected by the id from the request body.
+        /// </summary>
+        /// <param name="request">The request body from which the task is edited.</param>
+        /// <returns>A reference of the task entity.</returns>
         TaskEntity? UpdateTask(RequestOfManageTask request);
     }
 }
--- backend/ServiceContracts/ITaskService.cs
+++ backend/ServiceContracts/ITaskService.cs
@@ -5,10 +5,38 @@
 
     public interface ITaskService
     {
+        /// <summary>
+        /// Creates a new task.
+        /// </summary>
+        /// <param name="request">The request body from which the task is created.</param>
+        /// <returns>A response body of the created task.</returns>
         ResponseOfGetTasks.SingleTask AddNewTask(RequestOfManageTask request);
+        
+        /// <summary>
+        /// Removes a selected task.
+        /// </summary>
+        /// <param name="taskId">The id of the task selected to remove.</param>
+        /// <returns>A message confirmation of the task removal.</returns>
         string DeleteTask(int taskId);
+
+        /// <summary>
+        /// Modifies the task selected by the id from the request body.
+        /// </summary>
+        /// <param name="request">The request body from which the task is edited.</param>
+        /// <returns>A response body of the edited task.</returns>
         ResponseOfGetTasks.SingleTask? EditTask(RequestOfManageTask request);
+
+        /// <summary>
+        /// Gets the task by the provided id.
+        /// </summary>
+        /// <param name="taskId">The id of the task selected to retrieve.</param>
+        /// <returns>A response body of the retrieved task.</returns>
         ResponseOfGetTasks.SingleTask? GetTaskById(int taskId);
+
+        /// <summary>
+        /// Gets all the tasks.
+        /// </summary>
+        /// <returns>A response body containing the collection of tasks.</returns>
         ResponseOfGetTasks ListAllTasks();
     }
 }
